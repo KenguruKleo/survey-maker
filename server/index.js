31c58960f4b09c5d4e1f4e6ce28c38a7822930cc
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-08-15 15:42:54 +0300

    add connect to mongo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('../keys');
 require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect( keys.mongoURI, { useMongoClient: true } )
+    .then( db => console.log('Successfully connect to MongoDB') )
+    .catch( err => console.log('Error connecting to MongoDB: '+err) );
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT );
+console.log('Start application on PORT='+PORT);
+app.listen(PORT);
