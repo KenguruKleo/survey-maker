4b6303cce22bd19d65c6ba995ea3683c07d4b9ce
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-08-26 22:35:23 +0300

    show credits

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if( !req.req.isAuthenticated() ){
+    if( !req.isAuthenticated() ){
         return res.status(401).send({ error: 'You must log in!' });
     }
     next();
